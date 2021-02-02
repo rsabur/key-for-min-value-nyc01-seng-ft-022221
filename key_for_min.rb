3b3708ca0b48b357a8de4ecc2c53105200966e8d
Radiyah Abdus-Sabur <rsabur58@gmail.com>
2021-02-02 20:09:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,6 @@
 
 def key_for_min_value(name_hash)
 
-
-  name_hash.each do |key, value|
-    while row_index < name_hash.length do
-      if name_hash[row_index] < lowest
-        row_index = 0
-        lowest = 1000
-        lowest = name_hash[row_index]
-        row_index += 1
-      end
-    end
+name_hash.each do |key, value|
 
 end
